c52f2a11af2f0f531c66efd9cbb988357eacf20c
Roman_B <batrakov.r.v@yandex.ru>
2022-11-09 09:32:48 +0300

    ТЗ-15: final big changes

---
+++ src/main/java/ru/practicum/shareit/request/ItemRequest.java
@@ -0,0 +1,31 @@
+package ru.practicum.shareit.request;
+
+import lombok.*;
+import ru.practicum.shareit.user.User;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Table(name = "requests")
+public class ItemRequest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NonNull
+    @NotBlank
+    @Column(name = "description")
+    private String description;
+    @NonNull
+    @ManyToOne
+    @JoinColumn(name = "requestor_id", referencedColumnName = "id")
+    private User requestor;
+    @NonNull
+    @Column(name = "created")
+    private LocalDateTime created;
+}
